421380c516fdcb893bd79c7c538ccd1c1d8c3137
rishi-koul <rishikoul2@gmail.com>
2021-08-14 17:17:31 +0530

    Adding common messages to login and signup page

---
+++ pages/login.js
@@ -0,0 +1,12 @@
+import {HeaderMsg, FooterMsg} from "../components/Common/WelcomeMsg"
+
+function Login() {
+    return (
+        <>
+        <HeaderMsg />
+        <FooterMsg />
+        </>
+    )
+}
+
+export default Login
---
+++ pages/signup.js
@@ -0,0 +1,14 @@
+import {HeaderMsg, FooterMsg} from "../components/Common/WelcomeMsg"
+import React, {useState, useEffect, useRef} from "react"
+import {Form, Button, Message, Segment, TextArea, Divider} from "semantic-ui-react"
+
+function Signup() {
+    return (
+        <>
+        <HeaderMsg />
+        <FooterMsg />
+        </>
+    )
+}
+
+export default Signup
